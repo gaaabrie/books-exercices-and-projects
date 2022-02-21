6167a793718e8ce3525146cb4e1f32ed316efcab
gaaabrie <gaaaaabrie@gmail.com>
2022-02-21 14:12:06 -0300

    add

---
+++ C Programming, A Modern Approach, 2nd Edition/chapter 3/01/project_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{   int month = 0, day = 0, year= 0;
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf("%2d/%2d/%4d", &month, &day, &year);
+
+    printf("You entered the date, %4d%02d%02d", year, month, day);
+
+    return 0;
+}
