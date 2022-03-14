3677b5542707ec8c9645da42f609bb3ad98db51a
gaaabrie <gaaaaabrie@gmail.com>
2022-03-14 14:56:34 -0300

    fix

--- C Programming, A Modern Approach, 2nd Edition/chapter 5/09/09.c
+++ C Programming, A Modern Approach, 2nd Edition/chapter 5/09/09.c
@@ -8,7 +8,7 @@ int main(void){
     scanf("%d/%d/%d", &mon1, &day1, &year1);
     printf("Enter second date (mm/dd/yy):");
     scanf("%d/%d/%d", &mon2, &day2, &year2);
-
+ 
     if (year1 < year2)
         printf("%.2d/%.2d/%.2d %s %.2d/%.2d/%.2d", mon1, day1, year1, phrase, mon2, day2, year2);
     else if (year2 < year1)
